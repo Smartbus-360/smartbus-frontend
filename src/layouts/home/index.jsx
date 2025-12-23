549cf1b5bc6e6f1403f10b588ab0307964c7389a
Smartbus-360 <smartbus365@gmail.com>
2025-12-23 16:29:52 +0530

    Update index.jsx

--- src/layouts/home/index.jsx
+++ src/layouts/home/index.jsx
@@ -1,31 +1,16 @@
-// import React from 'react';
-// import { Outlet } from "react-router-dom";
-
-
-// export default function AuthLayout() {
-
-//   return (
-//     <div>
-//       <Outlet />
-//     </div>
-//   );
-// }
-
+import React from 'react';
 import { Outlet } from "react-router-dom";
-import Navbar from "../../components/navbar/home";
-import Footer from "../../components/footer/Footer";
 
-function HomeLayout() {
+
+export default function AuthLayout() {
+
   return (
-    <>
-      <Navbar />
-
-      {/* ✅ THIS WAS MISSING */}
+    <div>
       <Outlet />
-
-      <Footer />
-    </>
+    </div>
   );
 }
+
+
 
 export default HomeLayout;
