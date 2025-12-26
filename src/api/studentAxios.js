367a06b765939731d4d976ded671c8371cac0d42
Smartbus-360 <smartbus365@gmail.com>
2025-12-26 14:25:45 +0530

    Update studentAxios.js

--- src/api/studentAxios.js
+++ src/api/studentAxios.js
@@ -1,3 +1,25 @@
+// import axios from "axios";
+
+// const studentAxios = axios.create({
+//   baseURL: "https://api.smartbus360.com",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+// studentAxios.interceptors.response.use(
+//   response => response,
+//   error => {
+//     if (error.response?.status === 401) {
+//       localStorage.clear();
+//       window.location.href = "/student/login";
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+
+// export default studentAxios;
+
 import axios from "axios";
 
 const studentAxios = axios.create({
@@ -6,6 +28,20 @@ const studentAxios = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// ✅ Attach token automatically
+studentAxios.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("studentToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
+
+// ✅ Auto logout on 401
 studentAxios.interceptors.response.use(
   response => response,
   error => {
@@ -17,5 +53,5 @@ studentAxios.interceptors.response.use(
   }
 );
 
-
 export default studentAxios;
+
