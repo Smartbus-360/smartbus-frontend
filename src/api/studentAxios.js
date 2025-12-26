125852b9f2c74fbf646ce4802c11aa5f77e32e8a
Smartbus-360 <smartbus365@gmail.com>
2025-12-26 14:22:33 +0530

    Update studentAxios.js

--- src/api/studentAxios.js
+++ src/api/studentAxios.js
@@ -6,5 +6,16 @@ const studentAxios = axios.create({
     "Content-Type": "application/json",
   },
 });
+studentAxios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response?.status === 401) {
+      localStorage.clear();
+      window.location.href = "/student/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
 
 export default studentAxios;
