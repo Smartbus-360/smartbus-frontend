e02cba05b666843cf247a157861a12b8164c00ac
Smartbus-360 <smartbus365@gmail.com>
2026-02-11 10:38:37 +0530

    Update CoreValues.jsx

--- src/views/home/CoreValues.jsx
+++ src/views/home/CoreValues.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import Navbar from "../../components/navbar/home-nav";
 
 export default function CoreValues() {
   useEffect(() => {
@@ -8,7 +7,6 @@ export default function CoreValues() {
 
   return (
     <>
-      <Navbar />
 
       {/* Hero Section */}
       <section className="relative bg-green-600 text-white h-[50vh] flex items-center justify-center text-center">
